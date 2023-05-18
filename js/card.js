29b1fa951ecc99d7bb2a0b9943e6cec15598e275
Hakim <abdalhakem1226@gmail.com>
2023-05-18 13:55:56 +0300

    move to the details page relates #7

--- js/card.js
+++ js/card.js
@@ -2,57 +2,58 @@ function createCard(cards) {
 
   cards.forEach((card) => {
     // Create the <article> element
-    var article = document.createElement("article");
+    let article = document.createElement("article");
     article.className = "card";
 
-    article.onclick = (e) => {
-      window.location.href = '../pages/details.html'
-    }
+    const moveToDetails = document.createElement('a');
+    moveToDetails.href = '../pages/details.html'
+
+    article.appendChild(moveToDetails)
 
     // Create the <img> element
-    var img = document.createElement("img");
+    let img = document.createElement("img");
     img.src = card.image;
 
     // Append the <img> element to the <article> element
-    article.appendChild(img);
+    moveToDetails.appendChild(img);
 
     // Create the <div> element with class "card-info"
-    var cardInfo = document.createElement("div");
+    let cardInfo = document.createElement("div");
     cardInfo.className = "card-info";
 
     // Create the <h3> element with class "pre-title"
-    var preTitle = document.createElement("h3");
+    let preTitle = document.createElement("h3");
     preTitle.className = "pre-title";
     preTitle.textContent = card['pre-title'];
 
     // Create the <h2> element with class "title"
-    var title = document.createElement("h2");
+    let title = document.createElement("h2");
     title.className = "title";
     title.textContent = card.title;
 
     // Create the <div> element with class "rate"
-    var rate = document.createElement("div");
+    let rate = document.createElement("div");
     rate.className = "rate";
 
     // Create five <ion-icon> elements
-    for (var i = 0; i < 4; i++) {
-      var ionIcon = document.createElement("ion-icon");
+    for (let i = 0; i < 4; i++) {
+      let ionIcon = document.createElement("ion-icon");
       ionIcon.name = "star";
       rate.appendChild(ionIcon);
     }
 
     // Create the <ion-icon> element with class "star-outline"
-    var ionIconOutline = document.createElement("ion-icon");
+    let ionIconOutline = document.createElement("ion-icon");
     ionIconOutline.name = "star-outline";
     rate.appendChild(ionIconOutline);
 
     // Create the <p> element with class "author"
-    var author = document.createElement("p");
+    let author = document.createElement("p");
     author.className = "author";
     author.textContent = "Author: ";
 
     // Create the <span> element with class "author-name"
-    var authorName = document.createElement("span");
+    let authorName = document.createElement("span");
     authorName.className = "author-name";
     authorName.textContent = card.author;
 
@@ -66,7 +67,7 @@ function createCard(cards) {
     cardInfo.appendChild(author);
 
     // Append the <div> element with class "card-info" to the <article> element
-    article.appendChild(cardInfo);
+    moveToDetails.appendChild(cardInfo);
 
     // Append the <article> element to the desired parent element in your document
     cardsContinaer.appendChild(article);
